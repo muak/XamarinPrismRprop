45c6624e12c2b4707f5d8c8559e674eb3d41f7c2
Satoshi Nakamura <kamu@kamusoft-mini-2.local>
2019-12-01 13:56:05 +0900

    first commit

---
+++ XamarinPrismRprop/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Prism.Ioc;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Prism.Unity;
+using XamarinPrismRprop.ViewModels;
+using Prism;
+using XamarinPrismRprop.Views;
+
+namespace XamarinPrismRprop
+{
+    public partial class App:PrismApplication
+    {
+        public App(IPlatformInitializer initializer):base(initializer)
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnInitialized()
+        {
+            var result = await NavigationService.NavigateAsync("MainPage");
+
+            if (!result.Success)
+            {
+                System.Diagnostics.Debugger.Break();
+            }
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+        }
+    }
+}
---
+++ XamarinPrismRprop/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Prism.Mvvm;
+using Reactive.Bindings;
+
+namespace XamarinPrismRprop.ViewModels
+{
+    public class MainPageViewModel:BindableBase
+    {
+        public ReactivePropertySlim<string> Text { get; } = new ReactivePropertySlim<string>();
+
+        public MainPageViewModel()
+        {
+            Text.Value = "Welcome to Xamarin.Forms!";
+        }
+    }
+}
